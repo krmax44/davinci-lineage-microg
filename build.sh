4982e2e98a9492d954078c137fbd2456bb323588
krmax44 <hi@krmax44.de>
2022-05-17 10:33:13 +0200

    feat: improve upload script

--- build.sh
+++ build.sh
@@ -26,18 +26,23 @@ docker run \
    lineageos4microg/docker-lineage-cicd
 
 # create release
+files="srv/zips/$device/$branch-$tag-UNOFFICIAL-$device*"
+sha256=$(cat "srv/zips/$device/$branch-$tag-UNOFFICIAL-$device.zip.sha256sum" | grep -oP "^([a-f0-9]+)")
+
 read -N 1 -p "Create release? (y/n)" answer
 if ["$answer" == "y"]; then
-    gh release create "$tag"
+    gh release create \
+        --title "$branch-$tag-UNOFFICIAL-$device"
+        --notes "sha256: `$sha256`" 
+        "$tag"
+        $files
 fi
 
 # upload artifacts
-read -N 1 -p "Upload artifacts? (y/n)" answer
-if ["$answer" != "y"]; then exit 0; fi
-
-files="srv/zips/$device/$branch-$tag-UNOFFICIAL-$device*"
-for file in $files;
-do
-    rclone copy "$file" ota:/ --progress
-    gh release upload "$tag" "$file"
-done
+read -N 1 -p "Upload artifacts to OTA server? (y/n)" answer
+if ["$answer" == "y"]; then
+    for file in $files;
+    do
+        rclone copy "$file" ota:/ --progress
+    done
+ fi
